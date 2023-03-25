e55a009cc36b8e0d375c47f871b085589bf0a53b
jhonataT <jhonata.tenorio2526@gmail.com>
2023-03-25 18:48:55 -0300

    Creating Dashboard screen

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,8 +1,9 @@
-import { Bar, Line } from 'react-chartjs-2';
+import { Bar, Line, Doughnut, Pie, Radar } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
+  RadialLinearScale,
   LineElement,
   PointElement,
   BarElement,
@@ -10,29 +11,46 @@ import {
   Tooltip,
   Filler,
   Legend,
+  ArcElement
 } from 'chart.js';
-import { bar_options, mock_bar_data, area_options, area_data } from '../../core/charts';
+import { bar_options, mock_bar_data, area_options, area_data, donut_data, donut_options, multi_bar_data, multi_bar_options, pie_options, pie_data, radar_options, radar_data } from '../../core/charts';
 import './styles.css';
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
+  RadialLinearScale,
   BarElement,
   PointElement,
   LineElement,
   Filler,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  ArcElement
 );
 
-export const BarChart = ({ data }) => {
+export const BarChart = ({ data, multi }) => {
   return <div className="chart__container">
     <Bar
-      options={bar_options}
-      data={mock_bar_data}
+      options={multi ? multi_bar_options : bar_options}
+      data={multi ? multi_bar_data : mock_bar_data}
     />
   </div>
+}
+
+export const DonutChart = ({ data }) => {
+  return <Doughnut
+    data={donut_data}
+    options={donut_options}
+  />
+}
+
+export const PieChart = ({ data }) => {
+  return <Pie
+    data={pie_data}
+    options={pie_options}
+  />
 }
 
 export const AreaChart = ({ data }) => {
@@ -41,11 +59,21 @@ export const AreaChart = ({ data }) => {
   </div>
 }
 
+export const RadarChart = ({ data }) => {
+  return <div className="chart__container">
+    <Radar data={radar_data} options={radar_options} />
+  </div>
+}
+
 export const Charts = ({ type = 'bar', data }) => {
   const getChartByType = (type, data) => {
     switch(type) {
       case 'bar': return <BarChart data={data}/> 
       case 'area': return <AreaChart data={data} />
+      case 'donut': return <DonutChart data={data} />
+      case 'pie': return <PieChart data={data} />
+      case 'multi-bar': return <BarChart data={data} multi/>
+      case 'radar': return <RadarChart data={data} />
     }
   }
     
--- src/core/charts/index.js
+++ src/core/charts/index.js
@@ -19,7 +19,7 @@ export const mock_bar_data = {
 };
 
 export const bar_options = {
-  responsive: false,
+  responsive: true,
   plugins: {
     legend: {
       position: 'bottom',
@@ -32,7 +32,7 @@ export const bar_options = {
 };
 
 export const area_options = {
-  responsive: false,
+  responsive: true,
   plugins: {
     legend: {
       position: 'bottom',
@@ -49,10 +49,153 @@ export const area_data = {
   datasets: [
     {
       fill: true,
-      label: 'Número de usuários',
+      label: 'Usuários',
       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export const donut_options = {
+  responsive: true,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: {
+      position: 'bottom',
+    },
+    title: {
+      display: true,
+      text: 'Tipos de usuários',
+    },
+  },
+}
+
+export const donut_data = {
+  labels: ['Admin', 'Usuário Pro', 'Usuário normal'],
+  datasets: [
+    {
+      data: [25, 204, 974],
+      backgroundColor: [
+        'rgba(255, 64, 105, .7)',
+        'rgba(54, 162, 235, 0.7)',
+        'rgb(146, 191, 231, .7)',
+      ],
+      borderColor: [
+        'rgba(255, 64, 105, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgb(146, 191, 231, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export const pie_options = {
+  responsive: true,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: {
+      position: 'bottom',
+    },
+    title: {
+      display: true,
+      text: 'Aplicações e usuários',
+    },
+  },
+}
+
+export const pie_data = {
+  labels: ['Plataforma X', 'Aplicativo Y', 'Sistema Z'],
+  datasets: [
+    {
+      data: [25, 204, 974],
+      backgroundColor: [
+        'rgba(231, 158, 198, 0.7)',
+        'rgba(254, 213, 95, .7)',
+        'rgb(33, 170, 140, .7)',
+      ],
+      borderColor: [
+        'rgba(231, 158, 198, 1)',
+        'rgba(254, 213, 95, 1)',
+        'rgb(33, 170, 140, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export const multi_bar_data = {
+  labels,
+  datasets: [
+    {
+      label: 'Equipe 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgb(255, 99, 132)',
+      stack: 'Stack 1',
+    },
+    {
+      label: 'Equipe 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgb(53, 162, 235)',
+      stack: 'Stack 1',
+    },
+  ],
+};
+
+export const multi_bar_options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'Engajamento da equipe',
+    },
+    legend: {
+      position: 'bottom',
+    },
+  },
+  responsive: true,
+  interaction: {
+    mode: 'index',
+    intersect: false,
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+export const radar_options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'Média de atualizações',
+    },
+    legend: {
+      position: 'bottom',
+    },
+  },
+}
+
+export const radar_data = {
+  labels: ['Plataforma X', 'Aplicativo Y', 'Sistema Z'],
+  datasets: [
+    {
+      label: 'Atualizações diárias',
+      data: [24, 21, 15],
+      backgroundColor: 'rgba(255, 210, 204, 0.7)',
+      borderColor: 'rgba(255, 0, 0, 1)',
+      borderWidth: 1,
+    },
+    {
+      label: 'Atualizações mensais',
+      data: [112, 100, 123],
+      backgroundColor: 'rgba(249, 128, 18, 0.2)',
+      borderColor: 'rgba(249, 128, 18, 1)',
+      borderWidth: 1,
     },
   ],
 };
