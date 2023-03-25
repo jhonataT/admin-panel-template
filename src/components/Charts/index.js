05cb466efa03a02274514cadcf0a5c74bc8c5369
jhonataT <jhonata.tenorio2526@gmail.com>
2023-03-25 16:29:07 -0300

    Adding charts

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,36 +1,39 @@
 import { Bar } from 'react-chartjs-2';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 } from 'chart.js';
 import './styles.css';
 import { bar_options, mock_bar_data } from '../../core/charts';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 export const BarChart = ({ data }) => {
-    return <Bar options={bar_options} data={mock_bar_data} />;
+  return <Bar
+    height={90}
+    options={bar_options}
+    data={mock_bar_data}
+  />;
 }
 
 export const Charts = ({ type = 'bar', data }) => {
-    const getChartByType = (type, data) => {
-        switch(type) {
-            case 'bar': return <BarChart data={data}/> 
-        }
-
+  const getChartByType = (type, data) => {
+    switch(type) {
+      case 'bar': return <BarChart data={data}/> 
     }
+  }
     
-    return getChartByType(type)
+  return getChartByType(type)
 } 
