6b164c328a1d7def4dbcb05537d440d07fbd9f48
Caroline <carolineanjoss02@gmail.com>
2023-03-26 20:59:30 -0300

    improving new project's screen

--- src/screen/NewProjectsScreen/index.js
+++ src/screen/NewProjectsScreen/index.js
@@ -1,42 +1,68 @@
-import { Grid } from '@mui/material';
-import { DefaultButton } from '../../components/Button';
-import { Card } from "../../components/Card"
-import { DefaultInput } from "../../components/Input"
+import { FormHelperText, Grid } from "@mui/material";
+import { DefaultButton } from "../../components/Button";
+import { Card } from "../../components/Card";
+import { DefaultInput } from "../../components/Input";
+import "./styles.css";
 
 export const NewProjectsScreen = () => {
-  return <div className="new-projects__container">
-    <Card>
-      <DefaultInput width="fullWidth"
-        label="Nome do Projeto"
-        placeholder="Nome do Projeto"/>
-      <Grid container spacing={2}>  
-        <Grid item xs={6}>
-          <DefaultInput 
-            width="fullWidth"
-            label="Email do Usuário"
-            placeholder="Email"/>
-        </Grid>
+  return (
+    <div className="new-projects__container">
+      <Card>
+        <Grid container spacing={2}>
           <Grid item xs={6}>
-            <DefaultInput 
+        <DefaultInput
+          variant=""
+          width="fullWidth"
+          label="Nome do Projeto"
+          placeholder="Nome do Projeto"
+        />
+          </Grid>
+          <Grid item xs={6}>
+            <DefaultInput
+              width="fullWidth"
+              label="Email do Usuário"
+              placeholder="Email"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <DefaultInput
               width="fullWidth"
               label="Patrocinador"
-              placeholder="Patrocinador"/>
+              placeholder="Patrocinador"
+            />
           </Grid>
-            <DefaultButton
-              label="Cancelar"
-              handleClick
-              type = "contained"
-              color="error"
-              size="small"
-              fullWidth={false} />
-            <DefaultButton
-              label="Salvar"
-              handleClick
-              type = "contained"
-              color="success"
-              size="small"
-              fullWidth={false} />
-      </Grid>
-    </Card>
-  </div>
-}
+          <Grid item xs={3}>
+            <DefaultInput 
+            helperText="Início"
+            width="fullWidth" 
+            type="date" />
+          </Grid>
+          <Grid item xs={3}>
+            <DefaultInput 
+            helperText="Final"
+            width="fullWidth" 
+            type="date" />
+          </Grid>
+        </Grid>
+        <div className="buttons__container">
+          <DefaultButton
+            label="Cancelar"
+            handleClick
+            type="contained"
+            color="error"
+            size="small"
+            fullWidth={false}
+          />
+          <DefaultButton
+            label="Salvar"
+            handleClick
+            type="contained"
+            color="success"
+            size="small"
+            fullWidth={false}
+          />
+        </div>
+      </Card>
+    </div>
+  );
+};
