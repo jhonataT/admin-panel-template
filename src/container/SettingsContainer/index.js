2442a5043c5726ef81bd351403b5700cf50433a5
jhonataT <jhonata.tenorio2526@gmail.com>
2023-04-01 14:06:34 -0300

    Adding and fixing settings screen

--- src/container/SettingsContainer/index.js
+++ src/container/SettingsContainer/index.js
@@ -1,5 +1,34 @@
 import { SettingsScreen } from "../../screen/SettingsScreen";
 
+const settingOptions = [
+    {
+        step: "Usuário logado",
+        children: [
+            {
+                label: "Salvar login",
+                onToggle: () => console.log("AA"),
+                defaultChecked: false
+            },
+            {
+                label: "Mostrar notificações",
+                onToggle: () => console.log("AA"),
+                defaultChecked: true
+            }
+        ]
+    },
+    {
+        step: "Plataforma",
+        children: [
+            {
+                label: "Tema light",
+                onToggle: () => console.log("AA"),
+                defaultChecked: true,
+                disabled: true
+            },
+        ]
+    }
+]
+
 export const SettingsContainer = () => {
-    return <SettingsScreen />
+    return <SettingsScreen options={settingOptions}/>
 }
--- src/screen/SettingsScreen/index.js
+++ src/screen/SettingsScreen/index.js
@@ -1,7 +1,23 @@
+import { Typography } from '@mui/material';
+import { Card } from '../../components/Card';
+import { Toggle } from '../../components/Toggle';
 import './styles.css';
 
-export const SettingsScreen = () => {
-    return <main className="settings">
-        aaaaa
+export const SettingsScreen = ({ options = [] }) => {
+    return <main className="settings__container">
+        <Card>
+            {
+                Array.isArray(options) && options.map(item => (
+                  <div className="settings__content">
+                    <Typography mb={1} variant='h6'>{item.step}</Typography>
+                    {
+                        Array.isArray(item.children) && item.children.map(row => (
+                            <Toggle {...row}/>
+                        ))
+                    }
+                  </div>  
+                ))
+            }
+        </Card>
     </main>
 }
